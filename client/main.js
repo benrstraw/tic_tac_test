ccfd44bf5f8a207a5832d48f03e52b131f4a6e48
Benjamin R. Straw <benrstraw@gmail.com>
2020-10-25 14:32:30 -0400

    Implement basic JS structure.

--- client/main.js
+++ client/main.js
@@ -1,1 +1,32 @@
 var ws = new WebSocket("ws://wdc.benrstraw.xyz:4629")
+
+ws.onerror = function(error) {
+	console.error(error);
+};
+
+ws.onclose = function() {
+	console.log("Opened!");
+};
+
+ws.onopen = function() {
+	console.log("Closed!");
+};
+
+ws.onmessage = function(event) {
+	data = JSON.parse(event.data);
+	handle(data);
+};
+
+function send(cmd) {
+	var out = JSON.stringify(cmd);
+	console.log(out);
+	ws.send(out);
+}
+
+function handle(data) {
+	console.log(data);
+}
+
+function test(str) {
+	send({ 'test': str });
+}
